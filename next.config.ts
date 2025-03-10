e6bdb093bb864b818856c0dbc3d33e8a483f4650
Hamdi <hghayadha2001@gmail.com>
2025-03-10 03:20:56 +0100

    try to fix next config

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'export', // Enable static export
   images: {
-    unoptimized: true, // Fix image loading issues in static export
     domains: ['youtube.com', 'www.youtube.com', 'youtu.be'],
   },
 };
